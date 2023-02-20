140c327f4fe261fb66ab3f6761d26c8fd0a71647
Mohamed hassan <67841458+MohamedHassan1311@users.noreply.github.com>
2023-02-20 19:51:57 +0200

    update

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hyperpay_flutter_demo_v2/custom_ui.dart';
+import 'package:http/http.dart' as http;
+import 'package:hyperpay_flutter_demo_v2/ready_ui.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+
+
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Hyperpay Demo'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Hyperpay Flutter Demo",
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                child: Text('Ready UI'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Ready_UI(),
+                    ), ); },
+
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                child: Text('Custom UI'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => custom_UI()),
+                  );
+                },
+
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+    ;
+  }
+
+// Get battery level.
+
+
+
+}
